34458ab098e8f3f20b1c84b19ba19df21a0e7d74
ugurgull <ugurgul93@hotmail.com>
2023-12-20 10:10:20 +1100

    Asal sayi

---
+++ Asalsayi/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        for (int  i = 1; i <= 100; i++ ) {
+            int n = 0;
+            for (int k = 1; k <= i; k++){
+                if(i % k == 0){
+                    n++;
+                }
+            }
+            if(n <= 2){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
+
